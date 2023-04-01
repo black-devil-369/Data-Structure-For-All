9c167d56dc49f377c15423b31c398a670a13a2e0
black-devil-369 <ajaychauhan1262@gmail.com>
2023-04-01 11:54:19 -0400

    Singly Linkedlist using C++

--- SLL11.cpp
+++ SLL11.cpp
@@ -15,6 +15,7 @@ class SLL{
     void insertbegin(int);
     // function for inserting at the end
     void insertEnd(int);
+    void printlist();
 };
 SLL::SLL()
 {
@@ -23,9 +24,16 @@ SLL::SLL()
 void SLL::insertbegin(int data)
 {
     node *ajay = new node();
-    ajay->item = data;
-    ajay->next = NULL; // start;
-    start = ajay;
+    if(start==NULL){
+        ajay->item = data;
+        ajay->next = start;
+        start=ajay;
+    }
+    else{
+        ajay->item = data;
+        ajay->next = start->next;
+        start=ajay;
+    }
 }
 void SLL::insertEnd(int data)
 {
@@ -45,9 +53,21 @@ void SLL::insertEnd(int data)
         t->next = n;
     }
 }
+void SLL::printlist(){
+    node *t;
+    t=start;
+    while(t!=NULL){
+        cout<<t->item<<" ";
+        t=t->next;
+    }
+    cout<<endl;
+}
 // Derive Program
 int main(){
     SLL s1;
-    s1.insertbegin(34);
     s1.insertbegin(46);
+    s1.insertbegin(90);
+    s1.insertbegin(45);
+    s1.insertbegin(34);
+    s1.printlist();
 }
